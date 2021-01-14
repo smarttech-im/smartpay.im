d18a3133d519308afed8e208404a5dc09c4c7e41
liam gillings <liamgillings@outlook.com>
2021-01-14 23:02:12 +0000

    Add movement to the screen shot section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 (function ($) {
 
     "use strict";
@@ -20,94 +21,61 @@
         scrollSpeed: 900,
         animation: 'fade'
     });
-    /* testimonials Slider Active
-    =============================*/
-    $('.testimonials').owlCarousel({
-        loop: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 1000,
-        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
-        items: 1
-    });
-    /* testimonials Slider Active
-    =============================*/
-    $('.screen-slider').owlCarousel({
-        loop: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 1000,
-        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
-        items: 1,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        center: true,
-    });
-    /* testimonials Slider Active
-    =============================*/
-    $('.clients').owlCarousel({
-        loop: true,
-        margin: 30,
-        responsiveClass: true,
-        nav: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 1000,
-        navText: ['<i class="ti-arrow-left"></i>', '<i class="ti-arrow-right" ></i>'],
-        responsive: {
-            0: {
-                items: 3,
-            },
-            600: {
-                items: 4
-            },
-            1000: {
-                items: 6
-            }
-        }
-    });
-    /*--------------------
-       MAGNIFIC POPUP JS
-       ----------------------*/
-    var magnifPopup = function () {
-        $('.work-popup').magnificPopup({
-            type: 'image',
-            removalDelay: 300,
-            mainClass: 'mfp-with-zoom',
-            gallery: {
-                enabled: true
-            },
-            zoom: {
-                enabled: true, // By default it's false, so don't forget to enable it
 
-                duration: 300, // duration of the effect, in milliseconds
-                easing: 'ease-in-out', // CSS transition easing function
 
-                // The "opener" function should return the element from which popup will be zoomed in
-                // and to which popup will be scaled down
-                // By defailt it looks for an image tag:
-                opener: function (openerElement) {
-                    // openerElement is the element on which popup was initialized, in this case its <a> tag
-                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+    /* Scroll
+    ===================*/
+    let last_known_scroll_position = 0;
+    let ticking = false;
+    document.addEventListener('scroll', function(e) {
+        last_known_scroll_position = window.scrollY;
+      
+        if (!ticking) {
+          window.requestAnimationFrame(function() {
+            var elementList = document.getElementsByClassName("scroll-trigger");
+            if (elementList !== undefined) {
+                for (let index = 0; index < elementList.length; index++) {
+                    const element = elementList[index];
+                    if (element.hasAttribute('scroll-trigger')) {
+                        let eTop = element.getBoundingClientRect().top;
+                        let isInView = eTop > 0 && eTop < window.innerHeight;
+                        let className = element.getAttribute('scroll-trigger');
+
+                        if (isInView) {
+                            //console.log('Show');
+                            element.classList.add(className);
+                        } else {
+                            //console.log('Hide');
+                            element.classList.remove(className);
+                        }
+
+                        if (element.hasAttribute('scroll-trigger-alt-class')) {
+                            let altClass = element.getAttribute('scroll-trigger-alt-class');
+                            if (isInView) {
+                                element.classList.remove(altClass);
+                                
+                            } else {
+                                element.classList.add(altClass);
+                            }
+                        }
+        
+                        //console.log('scroll ' + last_known_scroll_position + ' item ' + e[0].getBoundingClientRect().top);
+                    }
                 }
             }
-        });
-    };
-    // Call the functions 
-    magnifPopup();
+
+
+            
+
+             
+            ticking = false;
+          });
+      
+          ticking = true;
+        }
+      });
 
     //Background Parallax
-    $('.header-area').parallax("50%", -0.4);
-    $('.price-area').parallax("50%", -0.5);
-    $('.testimonial-area').parallax("10%", -0.2);
-
 
     $('#accordion .panel-title a').prepend('<span></span>');
 
@@ -191,8 +159,7 @@
         });
 
 
-
-
+  
 
 
     /* Preloader Js
